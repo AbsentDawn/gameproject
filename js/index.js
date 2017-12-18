2bebf87210a8ceec52a247b1ce2409d13df1df00
Tech-A33 <Tech-A33@Tech-A33s-MacBook-Air.local>
2017-12-18 14:35:23 +0000

    Added ground

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	// //  Viewport Properties
+	// var viewport = $("#viewport");
+	// var viewportW = viewport.width();
+	// var viewportH = viewport.height();
+
+	// // Level id Properties 
+	// var level = $("#level");
+	// var levelW = level.width();
+	// var levelH = level.height();
+
+	// // Level scrolling variables 
+	// var levelSpeed = 1;
+	// var levelX = 0;
+
+	// $("button").click(function() {
+	// 	alert(levelW);
+	// });
+
+	
+	
+
+
+});

5b78110330c5b27b6117cee0fb6a94dc05036aa2
Tech-A33 <Tech-A33@Tech-A33s-MacBook-Air.local>
2017-12-19 16:13:00 +0000

    Collision detection improved

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 $(function() {
-
 	start();
 
 
 	// Functionk to start the game
 	function start(){
-
-		$("#enemy1").animate({left: '-300'}, 10000, checkCollisions);
+		$("#enemy1").animate({left: '-300'}, 10000);
 		jump();
 		player();
+		
+		setInterval(function() {
+			checkCollisions($("#enemy1"), $("#player"));
+		}, 20);
 	}
 
 	function player(){
@@ -29,39 +31,38 @@ $(function() {
 
 	function jump() {
 		 $("button").click(function() {
-			$("#player").animate({top: '15%'}, "slow", checkCollisions);
-			$("#player").animate({top: '50%'}, "slow", checkCollisions);
+			$("#player").animate({top: '15%'}, "slow");
+			$("#player").animate({top: '50%'}, "slow");
 
 		});
 	}
 
-	 // Collision detection
-	 function getPositions(box) {
-	   var $box = $(box);
-	   var pos = $box.position();
-	   var width = $box.width();
-	   var height = $box.height();
-	   return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-	 }
+
+	  // Collision detection
+	  function getPositions(box) {
+	    var $box = $(box);
+	    var pos = $box.position();
+	    var width = $box.width();
+	    var height = $box.height();
+	    return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
+	  }
 	        
-	 function comparePositions(p1, p2) {
-	   var x1 = p1[0] < p2[0] ? p1 : p2;
-	   var x2 = p1[0] < p2[0] ? p2 : p1;
-	   return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
-	 }
+	  function comparePositions(p1, p2) {
+	    var x1 = p1[0] < p2[0] ? p1 : p2;
+	    var x2 = p1[0] < p2[0] ? p2 : p1;
+	    return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
+	  }
 
-	 function checkCollisions(){
-	   var box = $("#enemy1");
-	   var pos = getPositions(box);
-
-	   var pos2 = getPositions(this);
-	   var horizontalMatch = comparePositions(pos[0], pos2[0]);
-	   var verticalMatch = comparePositions(pos[1], pos2[1]);            
-	   var match = horizontalMatch && verticalMatch;
-	   if (match) { 
-	   	console.log("Collision");
-	   }
-	   return !match;
-	 }
+	  function checkCollisions(enemy, player){
+	    var pos = getPositions(enemy);
+	    var pos2 = getPositions(player);
+	    var horizontalMatch = comparePositions(pos[0], pos2[0]);
+	    var verticalMatch = comparePositions(pos[1], pos2[1]);            
+	    var match = horizontalMatch && verticalMatch;
+	    if (match) { 
+	    	console.log("Collision");
+	    }
+	    return match;
+	  }
 
 });

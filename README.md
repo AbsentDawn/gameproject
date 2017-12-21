e77c6fd15c631637cbf2cfd92a05e61987c3afd7
Tech-A33 <Tech-A33@Tech-A33s-MacBook-Air.local>
2017-12-21 16:23:13 +0000

    Added Detailed Readme file

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Knight Run: Desolation of Bird Mama
-Feature Game for project week 
+# Game Project - Knight Run: Desolation of Bird Mama
+
+Knight Run: Desolation of Bird Mama is an endless runner inspired by many popular games such as Temple run and Subway Surfer. The aim of the game is to run and dodge dynamic obstacles trying to hinder your progress. The longer you run the higher your score. 
+
+Good Luck!
+
+
+## Technologies 
+
+A variety tools where used including software and programming languages. These include the following: 
+
+Software: 
+
+- Sublime Text
+- Google Chrome
+- Google Chrome Debugger
+
+Programming Langauges: 
+
+- HTML
+- CSS
+- Javascript/JQuery 
+
+
+## How to play 
+
+Aim: 
+
+- Player must run across the level for as long as possible. The longer the run the higher the score. Beware, birds will be storming through the screen trying to stop you. Make sure you avoid them. Good Luck! 
+
+Controls: 
+
+- Arrow Key Up: This allows the player to jump. The longer held down the higher the player goes.
+- Arrow Key Down: This allows the player to go under the ground. The longer held down the lower the player goes.
+- Arrow Key Right: This allows the player to accelerate. Will be needed to jump earlier to avoid multiple enemies grouped together.
+
+## Installation and Usage
+
+Cloning: 
+
+- To play the game via cloning, visit https://github.com/AbsentDawn/gameproject.
+Click on the "Clone or download" button and copy the url. Now you are able to clone off the terminal using the git command. Once cloned, enter the folders directory and open "index.html" and enjoy.
+
+Download: 
+
+- To play the game via download, visit https://github.com/AbsentDawn/gameproject. Click on the "Clone or download" button and select "Download zip". Once downloaded, extract the contents and open it. In the extracted folder select "index.html" and enjoy.
--- js/index.js
+++ js/index.js
@@ -1,30 +1,55 @@
 $(function() {
 
 	var $enemies = [];
-	var level = $("#level");
-	var levelW = level.width();
-	var levelH = level.height();
 	var score = 0;
-	var hasLost = true;
 	var movement = [];
+	var playerAnimation = player();
+	var enemyAnimation = enemy();
 
-	startButton();
+		var buttons = {
+		// Up key
+		38: {
+			top: "-=1"
+		}, 
 
-	// Function to start the game
+		// Right Key
+		39: {
+			left: "+=1"
+		},
+
+		// Down Key
+		40: {
+			top: "+=1"
+		}
+	}
+
+	// Start the game
+		startButton();
+
+	$(document).on({
+		keydown: keyDown, 
+		keyup: keyUp
+	});
+
+
+	
+
+	// Function running while in game
 	function start(){
 		score = 0;
 		$("#ground").removeAttr('style');
 		$("#background").removeAttr('style');
-
-		player();
+		// player();
 		// jump();
 		// accelerate();
 		// duck();
-		spawnEnemies();
+		spawnPlayer(playerAnimation);
+		spawnEnemies(enemyAnimation);
 		increaseScore();
-		// loseLogic();
+		loseLogic();
 	}
 
+	// start button
 	function startButton() {
 		$("#playbutton").click(function() {
 			playbutton.style.display = 'none';
@@ -32,7 +57,7 @@ $(function() {
 		});
 	}
 
-	// If player wins
+	// If player is hit
 	function loseLogic() {
 		$.each($enemies, function(index, value) {
 			var collisionDetected = collision($(value), $("#player"));
@@ -40,6 +65,7 @@ $(function() {
 		});	
 	}
 
+	// Reset game
 	function reset() {
 		// Set Score back to nil
 		score = 0;
@@ -49,12 +75,16 @@ $(function() {
 		$.each($enemies, function(index, value) {
 			value.remove();
 			$enemies = [];
-		})
+		});
 
+		// Keep everything static
 		setTimeout(function() {
 			$("#ground").css("animation", "0s");
 			$("#background").css("animation", "0s");
-			$("#player").clearQueue();
+			// $("#player").remove();
+			$("#player").dequeue();
+			// $("#player").clearQueue();
+			// keepPlayerBack();
 		}, 5);
 
 
@@ -62,6 +92,7 @@ $(function() {
 		playbutton.style.display = 'block';
 	}
 
+	// Check for collision
 	function collision(enemy, player) {
 		setInterval(function() {
 			if(checkCollisions(enemy, player)){
@@ -71,7 +102,13 @@ $(function() {
 		}, 20);
 	}
 
-	function spawnEnemies() {
+	function spawnPlayer(animation) {
+		$('<div>', {id: 'player'});
+		$("#level").append();
+	}
+
+	// Spawn Enemies
+	function spawnEnemies(animation) {
 		for(var i = 0; i < 4; i++){
 			// var enemy = $('#enemy').append('<div id="enemy"></div>');
 			$enemies.push($('<div>', { class: 'enemy', bird: 'bird' + i}));
@@ -79,13 +116,16 @@ $(function() {
 		}
 	}
 
+	// Increase score 
 	function increaseScore() {
 		setInterval(function() {
 			score++
 			$('#score').text('score: ' + score);
 		}, 100);		
-	}
+	}	
 
+
+	// The player
 	function player(){
 		 $("#player").animateSprite({
 		    fps: 12,
@@ -100,6 +140,24 @@ $(function() {
 		    }
 		});
 	}
+
+	// The enemy
+	function enemy(){
+		 $("#player").animateSprite({
+		    fps: 12,
+		    animations: {
+		        flyLeft: [4, 3, 2, 1, 0, 9, 8, 7, 6, 5, 14, 13, 12, 11]
+
+		    },
+
+		    loop: true,
+		    complete: function(){
+		        // use complete only when you set animations with 'loop: false'
+		        console.log("animation End");
+		    }
+		});
+	}
+
 
 
 	// function jump() {
@@ -163,33 +221,13 @@ $(function() {
 	// 	});
 	// }
 
-	var buttons = {
-		// Up key
-		38: {
-			top: "-=1"
-		}, 
-
-		// Right Key
-		39: {
-			left: "+=1"
-		},
-
-		// Down Key
-		40: {
-			top: "+=1"
-		}
-	}
-
-	$(document).on({
-		keydown: keyDown, 
-		keyup: keyUp
-	});
-
+	// Key down events
 	function keyDown(e) {
 		var key = e.which;
 		var animation = buttons[key];
 		if(!movement[key]) {
 			movement[key] = setInterval(theAnimation, 1);
+			console.log("up", key, movement[key]);
 		}
 
 		function theAnimation() {
@@ -197,16 +235,20 @@ $(function() {
 		}
 	}
 
+	// Key released events
 	function keyUp(e) {
 		var key = e.which
 		movement[key] = clearInterval(movement[key]);
 		$("#player").animate({top: '50%'}, "slow");
 		$("#player").animate({left: '0%'}, "slow");
+		console.log("up", key, movement[key]);
 	}
 
 
 	function keepPlayerBack() {
-
+		$("#player").animate({top: '50%'}, "fast");
+		$("#player").animate({left: '0%'}, "fast");
+		console.log("down", key, movement[key])
 	}
 
 	  // Collision detection
@@ -217,13 +259,15 @@ $(function() {
 	  var height = $box.height();
 	  return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
 	}
-	        
+	
+	 // Compare div positions 
 	function comparePositions(p1, p2) {
 	  var x1 = p1[0] < p2[0] ? p1 : p2;
 	  var x2 = p1[0] < p2[0] ? p2 : p1;
 	  return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
 	}
 
+	// Collision detection logic
 	function checkCollisions(enemy, player){
 	  var pos = getPositions(enemy);
 	  var pos2 = getPositions(player);
